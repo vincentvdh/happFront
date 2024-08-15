48d3b5bfd5da485da13c3ff766bd1138125647c6
Vincent Van den Heuvel <vincent.vandenheuvel@student.ap.be>
2024-08-15 07:10:49 +0200

    adds login page

---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { Card } from "antd"
+import Login from "../Components/Login"
+
+const LoginPage = () => {
+    return (
+        <Card>
+            <Login />
+        </Card>
+    )
+}
+
+export default LoginPage
